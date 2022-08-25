1b71970e75a959fa5b4998ef66dadb438aa55fcb
James Mwendwa <mwendwajames32@gmail.com>
2022-08-25 18:44:30 +0300

    jumia clone

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import MenuSidebar from "./MenuSidebar";
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>homepage</h2>
+      <MenuSidebar />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/MenuSidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MenuSidebar.css";
+
+function MenuSidebar() {
+  return (
+    <div className="menuSidebar">
+      <div className="menuLinks">
+        <a href="https://www.jumia.co.ke/groceries/">Supermarket</a>
+        <a href="">Health &amp; Beauty</a>
+        <a href="">Home &amp; Office</a>
+        <a href="">Phones &amp; Tablets</a>
+        <a href="">Computing</a>
+        <a href="">Electronics</a>
+        <a href="">Fashion</a>
+        <a href="">Gaming</a>
+        <a href="">Baby Products</a>
+        <a href="">Sporting Goods</a>
+        <a href="">Garden &amp; Outdoors</a>
+        <a href="">Other categories</a>
+      </div>
+    </div>
+  );
+}
+
+export default MenuSidebar;
