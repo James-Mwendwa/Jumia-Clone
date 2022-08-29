cfb92374b3ddafa591dcdecac091bab0507d6db2
James Mwendwa <mwendwajames32@gmail.com>
2022-08-29 18:57:24 +0300

    Jumia-Clone UI

--- src/Gaming.js
+++ src/Gaming.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Gaming.css";
+import PlayStationCard from "./PlayStationCard";
 
 function Gaming() {
   return (
-    <div className="gaming">
-      <img src="images/gaming/Gaming_Center.jpg" alt="gaming" />
+    <div className="container">
+      <div className="gaming">
+        <img src="images/gaming/Gaming_Center.jpg" alt="gaming" />
+      </div>
+
+      <PlayStationCard />
     </div>
   );
 }
---
+++ src/PlayStationCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./PlayStationCard.css";
+
+function PlayStationCard() {
+  return (
+    <div className="playStations">
+      <div className="playCard">
+        <img src="images/gaming/PS55.jpg" alt="PS5" />
+        <h3>PS5</h3>
+      </div>
+
+      <div className="playCard">
+        <img src="images/gaming/PS44.jpg" alt="PS5" />
+        <h3>PS5</h3>
+      </div>
+
+      <div className="playCard">
+        <img src="images/gaming/PS33.jpg" alt="PS5" />
+        <h3>PS5</h3>
+      </div>
+
+      <div className="playCard">
+        <img src="images/gaming/PSP.jpg" alt="PS5" />
+        <h3>PS5</h3>
+      </div>
+
+      <div className="playCard">
+        <img src="images/gaming/Xbox.jpg" alt="PS5" />
+        <h3>PS5</h3>
+      </div>
+
+      <div className="playCard">
+        <img src="images/gaming/XBOXONE.jpg" alt="PS5" />
+        <h3>PS5</h3>
+      </div>
+    </div>
+  );
+}
+
+export default PlayStationCard;
