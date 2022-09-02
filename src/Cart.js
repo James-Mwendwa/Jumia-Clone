048e5237a92ccaea9cacb20f0e9d5ff568d28589
James Mwendwa <mwendwajames32@gmail.com>
2022-09-02 14:24:56 +0300

    Cart UI

--- src/Cart.js
+++ src/Cart.js
@@ -8,28 +8,52 @@ import RemoveIcon from "@mui/icons-material/Remove";
 function Cart() {
   return (
     <div className="cart">
-      <div className="cartCard">
-        <div className="cartCount">
-          <p>Cart (2)</p>
-        </div>
-
-        <div className="itemCard">
-          <div className="itemCardLeft">
-            <img src="images/gaming/PS2.jpg" alt="" />
-            <span>
-              <DeleteOutlineIcon />
-              <button>REMOVE</button>
-            </span>
+      <div className="cartBanner">
+        <div className="cartCard">
+          <div className="cartCount">
+            <p>Cart (2)</p>
           </div>
 
-          <div className="itemCardRight">
-            <h3>Ksh 5,999</h3>
-            <div className="itemCardAdd">
+          <div className="itemCard">
+            <div className="itemCardLeft">
+              <div className="itemContainer">
+                <img src="images/gaming/PS2.jpg" alt="" />
+                <p>Fifa Pro</p>
+              </div>
+
               <span>
-                <RemoveIcon className="itemIcon" /> 1{" "}
-                <AddIcon className="itemIcon" />
+                <DeleteOutlineIcon />
+                <button>REMOVE</button>
               </span>
             </div>
+
+            <div className="itemCardRight">
+              <h3>Ksh 5,999</h3>
+              <div className="itemCardAdd">
+                <button>
+                  <RemoveIcon className="itemIcon" />
+                </button>
+                <span>1</span>
+                <button>
+                  <AddIcon className="itemIcon" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="amountCard">
+          <div className="cartSummary">
+            <p>CART SUMMARY</p>
+          </div>
+
+          <div className="subTotal">
+            <p>Subtotal</p>
+            <h2>Ksh 68,999</h2>
+          </div>
+
+          <div className="amountCheckout">
+            <button>CHECKOUT (Ksh 68,999)</button>
           </div>
         </div>
       </div>
