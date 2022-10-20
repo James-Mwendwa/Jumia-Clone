2facf9419a086cb9e7aeeffcbb2e65ebaec41103
James Mwendwa <mwendwajames32@gmail.com>
2022-10-20 20:52:53 +0300

    refactoring the code

--- src/TopSelling.js
+++ src/TopSelling.js
@@ -1,87 +1,31 @@
 import React from "react";
 import "./TopSelling.css";
 import AddCart from "./AddCart";
-import { products } from "./data/data";
+import { topProducts } from "./data/data";
 
 function TopSelling() {
   return (
     <div className="container1">
-      <div className="digitalGames1">
-        <div className="digitalGamesTop1">
+      <div className="topSelling">
+        <div className="topSellingTitle">
           <p>Top selling items</p>
         </div>
 
-        {products.map((product, index) => {
+        {topProducts.map((product, index) => {
           return (
-            <div className="digitalGamesCards1" key={product.index}>
-              <div className="gamesCard1">
+            <div className="topSellingCards" key={product.index}>
+              <div className="topSellingCard">
                 <img src={product.image} alt={product.image} />
                 <p>{product.title}</p>
-                <p>{product.price}</p>
-                <s>{product.price2}</s>
+                <p>Ksh {product.price}</p>
+                <s>Ksh {product.price2}</s>
               </div>
             </div>
           );
         })}
       </div>
-      {/* <div className="digitalGames1">
-        <div className="digitalGamesTop1">
-          <p>Top selling items</p>
-        </div>
 
-
-
-        <div className="digitalGamesCards1">
-          <div className="gamesCard1">
-            <img src="images/Top-selling/1.jpg" alt="" />
-            <p>Vision Plus Frameless...</p>
-            <p>Ksh 10,999</p>
-            <s>Ksh 16,500</s>
-            <AddCart />
-          </div>
-
-          <div className="gamesCard1">
-            <img src="images/Top-selling/2.jpg" alt="" />
-            <p>Techo CAMON</p>
-            <p>Ksh 22,999</p>
-            <s>Ksh 26,500</s>
-            <AddCart />
-          </div>
-
-          <div className="gamesCard1">
-            <img src="images/Top-selling/3.jpg" alt="" />
-            <p>Sony PSN Wallet Top Up...</p>
-            <p>Ksh 3,999</p>
-            <s>Ksh 4,800</s>
-            <AddCart />
-          </div>
-
-          <div className="gamesCard1">
-            <img src="images/Top-selling/4.jpg" alt="" />
-            <p>Generic Pop Station</p>
-            <p>Ksh 799</p>
-            <s>Ksh 1,500</s>
-            <AddCart />
-          </div>
-
-          <div className="gamesCard1">
-            <img src="images/Top-selling/5.jpg" alt="" />
-            <p>Generic BRICK LCD</p>
-            <p>Ksh 689</p>
-            <s>Ksh 1,378</s>
-            <AddCart />
-          </div>
-
-          <div className="gamesCard1">
-            <img src="images/Top-selling/6.jpg" alt="" />
-            <p>Otvo Dual PS4 Controller</p>
-            <p>Ksh 1,649</p>
-            <s>Ksh 3,000</s>
-            <AddCart />
-          </div>
-        </div>
-      </div>
-
+      {/* 
 
 
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
-export const products = [
+export const topProducts = [
   {
     id: 1,
     image: "images/Top-selling/1.jpg",
@@ -9,36 +9,36 @@ export const products = [
   {
     id: 2,
     image: "images/Top-selling/2.jpg",
-    title: "Vision Plus Frameless...",
-    price: 10999,
-    price2: 16500,
+    title: "Techo CAMON",
+    price: 22999,
+    price2: 26500,
   },
   {
     id: 3,
     image: "images/Top-selling/3.jpg",
-    title: "Vision Plus Frameless...",
-    price: 10999,
-    price2: 16500,
+    title: "Sony PSN Wallet Top Up...",
+    price: 3999,
+    price2: 4580,
   },
   {
     id: 4,
     image: "images/Top-selling/4.jpg",
-    title: "Vision Plus Frameless...",
-    price: 10999,
-    price2: 16500,
+    title: "Generic Pop Station",
+    price: 799,
+    price2: 1500,
   },
   {
     id: 5,
     image: "images/Top-selling/5.jpg",
-    title: "Vision Plus Frameless...",
-    price: 10999,
-    price2: 16500,
+    title: "Generic BRICK LCD",
+    price: 699,
+    price2: 1300,
   },
   {
     id: 6,
     image: "images/Top-selling/6.jpg",
-    title: "Vision Plus Frameless...",
-    price: 10999,
-    price2: 16500,
+    title: "Otvo Dual PS4 Controller",
+    price: 1699,
+    price2: 3000,
   },
 ];
